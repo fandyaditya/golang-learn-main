b1f1554f4a25404237e85c19dfb223e072697aa8
I Made Fandy Aditya Wirana <fandyadityaw@gmail.com>
2023-02-07 21:42:11 +0800

    Update module to latest version

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/fandyaditya/golang-learn-main
 
 go 1.20
 
-require github.com/fandyaditya/golang-learn-module v1.0.6
+require github.com/fandyaditya/golang-learn-module v1.1.0
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	sm := smartPhone.Samsung{ModelName: "Galaxy S21"}
 	xo := smartPhone.Xiaomi{ModelName: "Redmi Note 3000"}
 	sm.Nfc()
-	sm.Infrared()
+	//sm.Infrared() //this method call is obsolete in new version module
 	xo.Nfc()
 	xo.Bluetooth()
 }
